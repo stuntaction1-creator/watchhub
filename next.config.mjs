956a99a58e09c9f43237ad398607c1f05993f907
stuntaction1-creator <stuntaction1@gmail.com>
2026-04-21 21:49:56 +0900

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async rewrites() {
+  return {
+    beforeFiles: [
+      {
+        source: '/validation-key.txt',
+        destination: '/api/validation-key',
+      }
+    ],
+  };
+},
+
+export default nextConfig
